4a0b7bab175f75ae9dd9813812868ad8ddd58051
Mirza Abdijanovic <mirzaabdijanovic9@gmail.com>
2025-01-21 22:25:04 +0100

    Auth routes with passport

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,28 +3,95 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Hash;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ApiController extends Controller
 {
    public function register(Request $request) 
    {
+      $request->validate([
+         "name" => "required|string",
+         "email"=> "required|string|email|unique:users",
+         "password"=>"required|confirmed"
+      ]);
+      User::create([
+         "name" =>$request->name,
+         "email"=>$request->email,
+         "password"=>bcrypt($request->password),
+         "phone_number"=>$request->phone_number
+      ]);
 
+      return response()->json([
+         "status"=> 201,
+         "message"=>"User registered successfully"
+      ]);
    }
    public function login(Request $request) 
    {
+      $request->validate([
+         "email"=> "required|string|email",
+         "password"=>"required"
+      ]);
+
+      $user = User::where("email", $request->email)->first();
+
+      if(!empty($user)){
+         if(Hash::check($request->password, $user->password)){
+
+            $token = $user->createToken("myToken")->accessToken;
+
+            return response()->json([
+               "status"=> "200",
+               "message"=>"Logged in successfully",
+               "token"=>$token
+            ]);
+            
+         }else{
+            return response()->json([
+               "status"=> 422,
+               "message"=>"Passwords dint match"
+            ]);
+         }
+      }else{
+         return response()->json([
+            "status"=> 422,
+            "message"=>"User not found"
+         ]);
+      }
 
    }
-   public function progile(Request $request) 
+   public function profile() 
    {
-
+      $user = Auth::user();
+      return response()->json([
+         "status" => "200",
+         "message" =>"Logged in successfully",
+         "user" => $user
+      ]);
    }
-   public function refreshToken(Request $request) 
+   public function refreshToken() 
    {
+      $user = request()->user();
+      $token = $user->createToken("newToken");
 
+      $refreshToken = $token->accessToken;
+
+      return response()->json([
+         "status" => 201,
+         "message"=> "Refresh token",
+         "token"=> $refreshToken
+      ]);
    }
    public function logout(Request $request) 
    {
-
+      request()->user()->tokens()->delete();
+      
+      return response()->json([
+         "status" => 200,
+         "message"=> "User logged out"
+      ]);
    }
 }
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
+
+Route::post("register",[ApiController::class,"register"]);
+Route::post("login",[ApiController::class,"login"]);
+
+Route::group([
+    "middleware"=>["auth:api"]
+], function(){
+
+Route::get("profile",[ApiController::class,"profile"]);
+Route::get("refresh-token",[ApiController::class,"refreshToken"]);
+Route::get("logout",[ApiController::class,"logout"]);
+});
